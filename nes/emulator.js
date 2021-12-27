c2ba470577e8d6e72e4012a9c5b8c0a446e3692a
Marcos Santos <marcos.ts201128@gmail.com>
2021-12-26 21:57:13 -0300

    adicionada classe nesenumator

--- nes/emulator.js
+++ nes/emulator.js
@@ -1,5 +1,46 @@
-show_log_all = true
+//-------------------------------------------------------------
+//                          Emulator
+//-------------------------------------------------------------
+class NesEmulator extends Emulator{
 
-const NES_CPU = new NesCpu()
-const NES_PPU = new NesPpu()
-const NES_BUS = new NesBus(NES_CPU, NES_PPU)
+    onCreate(){
+        // Init nes components
+        this.cpu = new NesCpu()
+        this.ppu = new NesPpu(this.cpu)
+        this.bus = new NesBus(this.cpu, this.ppu)
+        this.screen = new Screen(this.cpu)
+
+        //this.bus.setCartridge()
+
+        // Init nes configs
+    }
+
+    // Override
+    async loadRom(file){
+        const array_buffer = await (new Response(file)).arrayBuffer()
+        this.source = new Uint8Array(array_buffer)
+
+        this.init( new NesCartridge(this.source) )
+    }
+
+    init(cartridge = this.bus.cart){
+        this.bus.setCartridge( cartridge )
+        
+        // clear ram
+        for (const i in this.bus.ram)
+            this.bus.ram[i] = 0x00
+
+        // load code to ram            
+        let offset = this.source_offset
+        for (const b of this.source) 
+            this.bus.ram[offset++] = b
+
+        // reset vectors
+        this.bus.ram[0xFFFC] = 0x00;
+        this.bus.ram[0xFFFD] = 0x80;
+
+        // reset
+        this.cpu.reset()
+        this.screen.init()
+    }
+}
